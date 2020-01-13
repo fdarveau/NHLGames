a888d1215c23fc178de9d0847be194cbadc47574
François Darveau <franklebest07@gmail.com>
2020-01-12 21:10:16 -0500

    add another flag to detect the MPC process more reliably for ad detection purposes

--- NHLGames/Utilities/AdDetection.vb
+++ NHLGames/Utilities/AdDetection.vb
@@ -171,10 +171,10 @@ Namespace Utilities
                 Where(Function(x) x.MainWindowTitle = "fd://0 - VLC media player" OrElse x.MainWindowTitle.ToLower().Contains(" @ ")).
                 Select(Function(x) x.Id)
             Dim mpc64Processes = Process.GetProcessesByName("MPC-HC64").
-                Where(Function(x) x.MainWindowTitle = "stdin" OrElse x.MainWindowTitle.ToLower().Contains(" @ ")).
+                Where(Function(x) x.MainWindowTitle = "stdin" OrElse x.MainWindowTitle.ToLower().Contains(" @ ") OrElse x.MainWindowTitle.ToLower().Contains("_complete.m3u8")).
                 Select(Function(x) x.Id)
             Dim mpc32Processes = Process.GetProcessesByName("MPC-HC").
-                Where(Function(x) x.MainWindowTitle = "stdin" OrElse x.MainWindowTitle.ToLower().Contains(" @ ")).
+                Where(Function(x) x.MainWindowTitle = "stdin" OrElse x.MainWindowTitle.ToLower().Contains(" @ ") OrElse x.MainWindowTitle.ToLower().Contains("_complete.m3u8")).
                 Select(Function(x) x.Id)
             Dim mpvProcesses = Process.GetProcessesByName("mpv").
                 Select(Function(x) x.Id)
